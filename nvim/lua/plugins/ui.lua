1d6f799d103e7dd7ddd6bdc22f0beca52f08b9f1
Mark Tzeng <mark.tzeng@broadcom.com>
2023-08-29 10:30:41 +0800

    nvim - remove unnecessary config for SmiteshP/nvim-navic

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -66,39 +66,4 @@ return {
       })
     end,
   },
-  {
-    "SmiteshP/nvim-navic",
-    opts = function()
-      return {
-        icons = {
-          File          = "",
-          Module        = "",
-          Namespace     = "",
-          Package       = "",
-          Class         = "",
-          Method        = "",
-          Property      = "",
-          Field         = "",
-          Constructor   = "",
-          Enum          = "",
-          Interface     = "",
-          Function      = "",
-          Variable      = "",
-          Constant      = "",
-          String        = "",
-          Number        = "",
-          Boolean       = "",
-          Array         = "",
-          Object        = "",
-          Key           = "",
-          Null          = "",
-          EnumMember    = "",
-          Struct        = "",
-          Event         = "",
-          Operator      = "",
-          TypeParameter = "",
-        },
-      }
-    end,
-  },
 }
