d170bc90cf422ccc76afdd56ee430c65f4af7dc7
Mark Tzeng <mark.tzeng@broadcom.com>
2023-12-25 15:39:11 +0800

    nvim: autocmd to the line I was on when I reopne a file

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,3 +12,9 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "c", "cpp" },
   command = "if line('$') > 10000 | setlocal diffopt=internal,filler,closeoff,context:99999 | endif",
 })
+
+-- Remember the line I was on when I reopne a file
+-- http://askubuntu.com/questions/202075/how-do-i-get-vim-to-remember-the-line-i-was-on-when-i-reopen-a-file
+vim.cmd [[
+  autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+]]
