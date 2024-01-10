b100456222b2eb901b2fe98dd05d0939b53b13b6
Mark Tzeng <mark.tzeng@broadcom.com>
2024-01-10 14:28:19 +0800

    nvim: remove autocmds for diffopt because it's a global option

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,12 +7,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
   command = "setlocal expandtab shiftwidth=2 tabstop=2",
 })
 
--- No diff folding to avoiding LSP performance problem
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
-  command = "if line('$') > 10000 | setlocal diffopt=internal,filler,closeoff,context:99999 | endif",
-})
-
 -- Remember the line I was on when I reopne a file
 -- http://askubuntu.com/questions/202075/how-do-i-get-vim-to-remember-the-line-i-was-on-when-i-reopen-a-file
 vim.cmd [[
