306579aed938903bc22e2cdca161234863e9f7e1
Mark Tzeng <mark.tzeng@broadcom.com>
2024-03-08 16:51:23 +0800

    nvim: extra symbols-outline replaced by outline

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -1,5 +1,5 @@
 return {
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.editor.navic" },
-  { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+  { import = "lazyvim.plugins.extras.editor.outline" },
 }
