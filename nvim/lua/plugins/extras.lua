48967b331737a3733e9a98602968d75ca38eaff0
Mark Tzeng <mt952679@c02f24jtmd6t.dhcp.broadcom.net>
2025-01-06 14:32:27 +0800

    nvim: disable mini-indentscope

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -1,5 +1,4 @@
 return {
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.editor.outline" },
-  { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
 }
