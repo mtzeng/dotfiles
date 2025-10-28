25f9d19097994077e23ee2859c7d3f2068a0bbbb
Mark Tzeng <yctzeng@gmail.com>
2025-10-28 16:58:54 +0800

    nvim: update lsp keymaps

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,23 +1,17 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- change a keymap - no reuse_win
-      keys[#keys + 1] = { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" }
-      -- change a keymap - use built-in lsp instead of telescope for loclist
-      keys[#keys + 1] = { "gr", function() vim.lsp.buf.references(nil, { loclist = true }) end, desc = "References" }
-      -- disable keymaps
-      keys[#keys + 1] = { "]]", false }
-      keys[#keys + 1] = { "[[", false }
-
-      -- disable lang servers
-      opts.servers['jsonls'] = nil
-      opts.servers['lua_ls'] = nil
-
-      -- disable inlay hints
-      -- https://github.com/LazyVim/LazyVim/discussions/3191
-      opts.inlay_hints.enabled = false
-    end,
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ['*'] = {
+        keys = {
+          -- Change an existing keymap
+          -- use built-in lsp instead of telescope for loclist
+          { "gr", function() vim.lsp.buf.references(nil, { loclist = true }) end, desc = "References" },
+          -- Disable a keymap
+          { "]]", false },
+          { "[[", false },
+        },
+      },
+    },
   },
 }
